19cd24b9c07adafc62566c8c8af6628a78023d30
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-23 08:47:40 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import {fetchCharacters} from './services/fetchCharacters';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      characters: []
+    }
+
+
+  }
+
+  componentDidMount() {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    fetchCharacters()
+    .then(data => {
+      this.setState({
+        characters: data
+      });
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        :D
+      <div className="app">
+        
       </div>
     );
   }
