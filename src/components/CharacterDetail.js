bec0751be6ce84c593db21850afb1e0e88ba3b76
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-23 12:26:28 +0200

    propTypes in detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const CharacterDetail = props => {
@@ -38,5 +39,11 @@ const CharacterDetail = props => {
 
 
 };
+
+CharacterDetail.propTypes = {
+	routerProps: PropTypes.object.isRequired,
+	characters: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
 
 export default CharacterDetail
