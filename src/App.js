ca6fe7d38b2a2cf5a2fa49e0810667d0a2de03d2
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-23 09:19:44 +0200

    paint the characters

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      characters: []
+      characters: [],
+      user: ''
     }
 
+    this.getUserInput = this.getUserInput.bind(this);
 
   }
 
@@ -21,15 +23,39 @@ class App extends React.Component {
     fetchCharacters()
     .then(data => {
       this.setState({
-        characters: data
+        characters: data.results
       });
     });
+  }
+
+  getUserInput(event) {
+    const value = event.currentTarget.value;
+
+    this.setState({
+      user: value
+    })
   }
 
   render() {
     return (
       <div className="app">
-        
+        <header className="app__t"><h1 className="title">Rick and Morty</h1></header>
+        <main className="app__main">
+          <input type="text" className="userInput" onChange={this.getUserInput} />
+          <ul className="characters-list">
+            {this.state.characters.map(item => {
+              return (
+                <li className="character-name" key={item.id}>
+                  <div className="character-card">
+                    <img src={item.image} alt={item.name} className="character-img"/>
+                    <h2 className="character-name">{item.name}</h2>
+                    <p className="character-specie">{item.species}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
       </div>
     );
   }
