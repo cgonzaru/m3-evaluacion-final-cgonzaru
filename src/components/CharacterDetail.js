ea441e1783925ae1a69d7ea2ddd30270927d5123
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-23 12:22:37 +0200

    paint character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const CharacterDetail = props => {
+	const { routerProps, characters } = props;
+	const characterId = parseInt(routerProps.match.params.characterId);
 
-	return (
-		<div className="detail">
-			Soy el detalle
-			<Link to="/" className="home">Vuelve al listado</Link>
-    </div>
-	);
+	if (characterId > characters.length) {
+		return (
+			<div>
+				<p>El número del detalle excede el número de personajes </p>
+				<Link to="/" className="app__back">Vuelve al listado</Link>
+			</div>
+		);
+	}
+
+	const character = characters.filter(item => item.id === characterId);
+
+	if (character[0]) {
+		const { image, name, status, species, origin, episode } = character[0];
+		return (
+			<div className="character-detail">
+				<Link to="/" className="app__back">Volver</Link>
+				<div className="character-info">
+					<img src={image} alt={name} className="character-img" />
+					<div className="info">
+						<h2 className="character-name">{name}</h2>
+						<p className="character-status">Status: {status}</p>
+						<p className="character-status">Species: {species}</p>
+						<p className="character-status">Origin: {origin.name}</p>
+						<p className="character-status">Episodes: {episode.length}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+
 
 };
 
--- src/services/fetchCharacters.js
+++ src/services/fetchCharacters.js
@@ -1,4 +1,5 @@
-const endpoint = 'https://rickandmortyapi.com/api/character/';
+const endpoint = 'https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json';
+/* https://rickandmortyapi.com/api/character/ */
 
 const fetchCharacters = () => {
   return fetch(endpoint).then(response => response.json());
