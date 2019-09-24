f0df9b506c97ef3282ded047f5ac098823b02d6c
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-24 12:16:40 +0200

    generate docs

---
+++ docs/precache-manifest.8d904bb03cb3238b6eca8cb32dd4f7b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095d981b765e5ebe82b0231d7674e040",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58012928ba5263e018f8",
+    "url": "./static/css/main.a2a82c9d.chunk.css"
+  },
+  {
+    "revision": "da8acb4f379fb1fdb204",
+    "url": "./static/js/2.550cfd40.chunk.js"
+  },
+  {
+    "revision": "58012928ba5263e018f8",
+    "url": "./static/js/main.e756ab9c.chunk.js"
+  },
+  {
+    "revision": "7a39c2b4817586ba9759",
+    "url": "./static/js/runtime-main.148dbb40.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
